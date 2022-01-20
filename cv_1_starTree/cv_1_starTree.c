5fc57590e05ba2b8fac811cade5b8df936ce031d
Jan Podávka <jan.podavka@tul.cz>
2022-01-21 00:31:03 +0100

    start

--- cv_1_starTree/cv_1_starTree.c
+++ cv_1_starTree/cv_1_starTree.c
@@ -1,6 +1,10 @@
 ﻿#include <stdio.h>
 
 int main() {
-	printf("Prvni cviceni");
+	printf("Prvni cviceni - strom ");
+	int vel;
+	printf("Zadej velikost pyramidy: ");
+	scanf("%d", &vel);
+	prinf(" velikost je: %d", vel);
 	return 0;
 }

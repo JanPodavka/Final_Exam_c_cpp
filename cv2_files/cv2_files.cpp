0e529529bee3c997888ca75e20bb6955df8b075c
Jan Podávka <jan.podavka@tul.cz>
2022-01-21 02:14:14 +0100

    cv2_files

---
+++ cv2_files/cv2_files.cpp
@@ -0,0 +1,10 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+
+	printf("test");
+}
+
---
+++ cv_1_starTree/cv_1_starTree.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+	printf("Prvni cviceni - strom ");
+	int vel;
+	printf("Zadej velikost pyramidy: ");
+	scanf("%d", &vel);
+	printf(" velikost je: %d \n", vel);
+
+	//pyramidy
+
+	for (int i = 0; i < vel; i++) {
+
+		for (int k = vel; k < vel + i; k++) {
+			printf(" ");
+		}
+			
+		for (int j = 0; j < vel - i; j++) {
+			printf("* ");
+		}
+		printf("\n");
+
+	}
+		
+	//otočená pyramida
+
+	for (int i = vel; i >= 0; i--) {
+
+		for (int k = vel; k < vel + i; k++) {
+			printf(" ");
+		}
+
+		for (int j = 0; j < vel - i; j++) {
+			printf("* ");
+		}
+		printf("\n");
+
+	}
+
+	return 0;
+} 

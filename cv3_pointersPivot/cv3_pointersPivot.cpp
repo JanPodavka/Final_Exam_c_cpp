2ea9e71ca1d7245f11dc2d95eba2e7ecbfc39e95
Jan Podávka <jan.podavka@tul.cz>
2022-01-21 19:37:59 +0100

    pivot minus (skipped)

--- cv3_pointersPivot/cv3_pointersPivot.cpp
+++ cv3_pointersPivot/cv3_pointersPivot.cpp
@@ -1,6 +1,20 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+//Napište program, který bude umět přehodit vybraný datový typ (integer, long integer, double) okolo pivotu 
+//(prostřední byte(s) datového typu).
+
 
 int main()
 {
-	printf("test");
+	printf("test\n");
+	int c = 123456;
+	int* pc = &c;
+
+	printf("%d\n", *pc);
+	
+
+
+
+
+
 }

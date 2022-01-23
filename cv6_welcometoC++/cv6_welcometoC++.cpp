910ad25b1698d620076babe4ba8379fa8eb83e85
Jan Podávka <jan.podavka@tul.cz>
2022-01-23 03:23:05 +0100

    cv6 C++ - arrays and myString

---
+++ cv6_welcometoC++/cv6_welcometoC++.cpp
@@ -0,0 +1,17 @@
+﻿// cv6_welcometoC++.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+//Napište program v C++, který bude realizovat metody pro práci s řetězcem(pomocí manipulace s jednotlivými znaky).
+//Napište metody pro :
+//alokaci základního pole znaků(string)
+//získání délky řetězce
+//výpis řetězce na obrazovku
+//vložení / přidání znaku do řetězce
+//substring(int start, int end)
+//to_capitals() - převod všech znaků na velká písmena
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
